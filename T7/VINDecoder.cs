d6b9a4d28319e1eb25c89f87dc15108cef4b4782
mattiasclaesson <mattiaserikanna@gmail.com>
2014-02-10 23:17:21 +0100

    Added latest vindecoder from t8suite

--- T7/VINDecoder.cs
+++ T7/VINDecoder.cs
@@ -11,11 +11,13 @@ namespace T7
         Saab9000 = 2,
         Saab900SE = 3,
         Saab93 = 4,
-        Saab95 = 5,
-        Saab99 = 6,
-        OpelSignum,
-        OpelVectra,
-        CadillacBTS
+        Saab93new = 5,
+        Saab95 = 6,
+        Saab95new = 7,
+        Saab99 = 8,
+        OpelSignum = 9,
+        OpelVectra = 10,
+        CadillacBTS = 11
     }
 
     public enum VINEngineType : int
@@ -49,29 +51,46 @@ namespace T7
         B308I,
         B204I,
         B204S,
-        B207R,
-        B207L,
-        B207E,
+        B207F,
+        B207G,
+        B207H,
+        B207R, /* 2.0T */
+        B207S,
+        B207L, /* 2.0t */
+        B207E, /* 1.8t */
+        B207M,
         Z20NET,
         D223L,
         D308L,
-        Z18XE,
-        Z19DT,
-        Z19DTH,
+        Z18XE, /* 1.8i */
+        Z19DT, /* 1.9TiD, 8-valve */
+        Z19DTH, /* 1.9TiD, 16-valve */
         Z19DTR,
-        B284L,
+        B284L, /* 2.8t */
         B284E,
-        B284R
+        B284R,
+        Z19DTR_130,
+        Z19DTR_160,
+        Z19DTR_180,
+        A20NFT, /* Turbo4 */
+        A28NET_LBW,
+        A28NER_LAU,
+        A20DTH_LBS,
+        A20DTR_LBY,
+        A20NHT_LDK
     }
 
     public enum VINTurboModel : int
     {
+        Unknown,
         None,
         GarretT25,
         GarretTB2529,
         GarretTB2531,
-        MitsuTD04,
-        GarrettT17
+        MitsubishiTD04,
+        GarrettT17,
+        GarrettGT2052,
+        MitsubishiTD04L_11TK
     }
 
     public class VINDecoder
@@ -80,56 +99,54 @@ namespace T7
         {
             // Pos 1-3 World Manufacturer Identifier
             // Pos 1 Geographical Area: Y=Northern Europe
-            // Pos 2 Country: S=Sweden
+            // Pos 2 Country: S=Sweden K=Finland
             // Pos 3 Car Manuacturer: 3=Saab Automobile AB
             // 
             VINCarInfo _carInfo = new VINCarInfo();
-            if (VINNumber.StartsWith("YK1")) // finland?
+            if (VINNumber.StartsWith("YK1") || // finland
+                VINNumber.StartsWith("YS3"))  // sweden
             {
                 _carInfo.Makeyear = DecodeMakeyear(VINNumber);
                 _carInfo.CarModel = DecodeCarModel(VINNumber);
-                _carInfo.EngineType = DecodeEngineType(VINNumber);
+                _carInfo.EngineType = DecodeEngineType(VINNumber, _carInfo.CarModel, _carInfo.Makeyear);
                 _carInfo.PlantInfo = DecodePlantInfo(VINNumber);
-                _carInfo.Series = DecodeSeries(VINNumber);
+                _carInfo.Series = DecodeSeries(VINNumber, _carInfo.CarModel, _carInfo.Makeyear);
                 _carInfo.Body = DecodeBodyType(VINNumber);
-                _carInfo.TurboModel = DecodeTurboModel(_carInfo.EngineType, _carInfo.CarModel);
+                _carInfo.TurboModel = DecodeTurboModel(_carInfo.EngineType, _carInfo.CarModel, _carInfo.Makeyear);
                 _carInfo.GearboxDescription = DecodeTransmissionType(VINNumber);
                 _carInfo.Valid = true;
             }
-            else if (VINNumber.StartsWith("YS3")) // sweden?
+            else if (VINNumber.StartsWith("YSC")) // Cadillac
             {
                 _carInfo.Makeyear = DecodeMakeyear(VINNumber);
-                _carInfo.CarModel = DecodeCarModel(VINNumber);
-                _carInfo.EngineType = DecodeEngineType(VINNumber);
+                _carInfo.CarModel = VINCarModel.CadillacBTS;
+                _carInfo.EngineType = DecodeEngineType(VINNumber, _carInfo.CarModel, _carInfo.Makeyear);
                 _carInfo.PlantInfo = DecodePlantInfo(VINNumber);
-                _carInfo.Series = DecodeSeries(VINNumber);
+                _carInfo.Series = DecodeSeries(VINNumber, _carInfo.CarModel, _carInfo.Makeyear);
                 _carInfo.Body = DecodeBodyType(VINNumber);
-                _carInfo.TurboModel = DecodeTurboModel(_carInfo.EngineType, _carInfo.CarModel);
+                _carInfo.TurboModel  = VINTurboModel.Unknown;
                 _carInfo.GearboxDescription = DecodeTransmissionType(VINNumber);
                 _carInfo.Valid = true;
-            }
-            else if (VINNumber.StartsWith("YSC")) // Cadillac?
-            {
-                _carInfo.Makeyear = DecodeMakeyear(VINNumber);
-                _carInfo.CarModel = VINCarModel.CadillacBTS;//DecodeCarModel(VINNumber);
-                _carInfo.EngineType = DecodeEngineType(VINNumber);
-                _carInfo.PlantInfo = "";// DecodePlantInfo(VINNumber);
-                _carInfo.Series = "";// DecodeSeries(VINNumber);
-                _carInfo.Body = "";// DecodeBodyType(VINNumber);
-                _carInfo.TurboModel = VINTurboModel.None;// DecodeTurboModel(_carInfo.EngineType, _carInfo.CarModel);
-                _carInfo.GearboxDescription = ""; //DecodeTransmissionType(VINNumber);
-                _carInfo.Valid = true;
+                /*
+                Engines:
+                1.9 L Fiat turbodiesel I4 16v, 150 hp (110 kW)
+                1.9 L Fiat turbodiesel I4 16v, 180 hp (132 kW) (2007-)
+                2.0 L Ecotec LK9 I4, mid-pressure turbo, 175 hp (129 kW)
+                2.0 L Ecotec LK9 I4, high-pressure turbo, 210 hp (154 kW)
+                2.0 T FlexPower 200 hp (147 kW)
+                2.8 L HFV6 V6, turbo, 250 hp (184 kW)
+                */
             }
 
             else if (VINNumber.StartsWith("W0L")) // Opel
             {
                 _carInfo.Makeyear = DecodeMakeyear(VINNumber);
                 _carInfo.CarModel = DecodeCarModelOpel(VINNumber);
-                _carInfo.EngineType = VINEngineType.Z20NET;// DecodeEngineType(VINNumber);
-                _carInfo.PlantInfo = "";// DecodePlantInfo(VINNumber);
-                _carInfo.Series = DecodeSeries(VINNumber);
+                _carInfo.EngineType = VINEngineType.Z20NET;
+                _carInfo.PlantInfo = "";
+                _carInfo.Series = DecodeSeries(VINNumber, _carInfo.CarModel, _carInfo.Makeyear);
                 _carInfo.Body = DecodeBodyType(VINNumber);
-                _carInfo.TurboModel = DecodeTurboModel(_carInfo.EngineType, _carInfo.CarModel);
+                _carInfo.TurboModel = DecodeTurboModel(_carInfo.EngineType, _carInfo.CarModel, _carInfo.Makeyear);
                 _carInfo.GearboxDescription = DecodeTransmissionType(VINNumber);
                 _carInfo.Valid = true;
             }
@@ -144,7 +161,7 @@ namespace T7
             else return VINCarModel.Unknown;
         }
 
-        private VINTurboModel DecodeTurboModel(VINEngineType vINEngineType, VINCarModel carModel)
+        private VINTurboModel DecodeTurboModel(VINEngineType vINEngineType, VINCarModel carModel, int makeYear)
         {
             // depending on enginetype and vehicletype, determine turbo type
             switch (vINEngineType)
@@ -165,17 +182,39 @@ namespace T7
                         return VINTurboModel.GarretT25;
                     }
                 case VINEngineType.B205R:
-                    return VINTurboModel.MitsuTD04;
+                    return VINTurboModel.MitsubishiTD04;
                 case VINEngineType.B234E:
                 case VINEngineType.B234L:
                     return VINTurboModel.GarretT25;
                 case VINEngineType.B234R:
-                    return VINTurboModel.MitsuTD04;
+                    return VINTurboModel.MitsubishiTD04;
                 case VINEngineType.B235E:
-                case VINEngineType.B235L:
                     return VINTurboModel.GarrettT17;
+                case VINEngineType.B235L:
                 case VINEngineType.B235R:
-                    return VINTurboModel.MitsuTD04;
+                    return VINTurboModel.MitsubishiTD04;
+                case VINEngineType.B207E:
+                case VINEngineType.B207L:
+                    if (makeYear > 2006)
+                        return VINTurboModel.MitsubishiTD04L_11TK;
+                    else
+                        return VINTurboModel.GarrettGT2052;
+                case VINEngineType.B207R:
+                    return VINTurboModel.MitsubishiTD04;
+                case VINEngineType.B284R:
+                    return VINTurboModel.MitsubishiTD04;
+                case VINEngineType.A28NER_LAU:
+                    return VINTurboModel.Unknown;
+                case VINEngineType.B207H:
+                case VINEngineType.B207G:
+                case VINEngineType.B207S:
+                case VINEngineType.B207M:
+                case VINEngineType.B207F:
+                    return VINTurboModel.Unknown;
+                case VINEngineType.A20NFT:
+                    return VINTurboModel.Unknown;
+                case VINEngineType.Z20NET:
+                    return VINTurboModel.Unknown;
             }
             return VINTurboModel.None;
         }
@@ -227,9 +266,40 @@ namespace T7
              * */
         }
 
-        private string DecodeSeries(string VINNumber)
+        private string DecodeSeries(string VINNumber, VINCarModel carModel, int makeYear)
         {
             if (VINNumber.Length < 5) return string.Empty;
+            else if (carModel == VINCarModel.Saab93new)
+            {
+                if (makeYear == 2008)
+                {
+                    if (VINNumber[4] == 'M') return "Saab 9-3 TurboX";
+                }
+                else if (makeYear > 2009)
+                {
+                    if (VINNumber[4] == 'A') return "Saab 9-3 Linear";
+                    else if (VINNumber[4] == 'B') return "Saab 9-3 Vector";
+                    else if (VINNumber[4] == 'C') return "Saab 9-3 Aero";
+                    else if (VINNumber[4] == 'D') return "Saab 9-3 X";
+                    else if (VINNumber[4] == 'E') return "Saab 9-3 Linear Convertible";
+                    else if (VINNumber[4] == 'F') return "Saab 9-3 Vector Convertible";
+                    else if (VINNumber[4] == 'G') return "Saab 9-3 Aero Convertible";
+                }
+                else
+                {
+                    if (VINNumber[4] == 'B') return "Saab 9-3 Linear";
+                    else if (VINNumber[4] == 'D') return "Saab 9-3 Arc";
+                    else if (VINNumber[4] == 'F') return "Saab 9-3 Vector";
+                    else if (VINNumber[4] == 'H') return "Saab 9-3 Aero";
+                }
+            }
+            else if (carModel == VINCarModel.Saab95new)
+            {
+                if (VINNumber[4] == 'N') return "Saab 9-5 Linear";
+                else if (VINNumber[4] == 'P') return "Saab 9-5 Vector";
+                else if (VINNumber[4] == 'R') return "Saab 9-5 Aero";
+                else return string.Empty;
+            }
             else if (VINNumber[4] == 'A') return "Model series I, Driver airbag";
             else if (VINNumber[4] == 'B') return "Model series I, Driver and passenger airbags";
             else if (VINNumber[4] == 'C') return "Model series II, Driver airbag";
@@ -245,7 +315,8 @@ namespace T7
             else if (VINNumber[4] == 'Y') return "All cars without passive restraint system";
             else if (VINNumber[4] == 'K') return "Linear, Driver and passenger airbags";
             else if (VINNumber[4] == 'L') return "Vector, Driver and passenger airbags";
-            else return string.Empty;
+
+            return string.Empty;
             /*
             B = i 
             C = i16 
@@ -259,6 +330,7 @@ namespace T7
             else if (VINNumber[10] == '1') return "Trollhättan line A (9-3)";
             else if (VINNumber[10] == '2') return "Trollhättan line B (900 / 9-3)";
             else if (VINNumber[10] == '3') return "Trollhättan line A (9-5)";
+            else if (VINNumber[10] == '4') return "Trollhättan (9-5)";
             else if (VINNumber[10] == '5') return "Malmö, Sweden";
             else if (VINNumber[10] == '6') return "Graz, Austria (9-3 convertible)";
             else if (VINNumber[10] == '7') return "Nystad, Finland (900 / 9-3)";
@@ -285,15 +357,77 @@ namespace T7
             else if (VINNumber[3] == 'C') return VINCarModel.Saab9000;
             else if (VINNumber[3] == 'D') return VINCarModel.Saab93; // code=9000
             else if (VINNumber[3] == 'E') return VINCarModel.Saab95; // code=9600
-            else if (VINNumber[3] == 'F') return VINCarModel.Saab93; // code=9400
-            else if (VINNumber[3] == 'G') return VINCarModel.Saab95; // code=650
+            else if (VINNumber[3] == 'F') return VINCarModel.Saab93new; // code=9400
+            else if (VINNumber[3] == 'G') return VINCarModel.Saab95new; // code=650
             else return VINCarModel.Unknown;
         }
 
-        private VINEngineType DecodeEngineType(string VINNumber)
+        private VINEngineType DecodeEngineType(string VINNumber, VINCarModel carModel, int makeYear)
         {
             if (VINNumber.Length < 8) return VINEngineType.Unknown;
-            else if (VINNumber[7] == 'A') return VINEngineType.B235L;
+            else if (makeYear > 2010 && carModel == VINCarModel.Saab93new)
+            {
+                //For 9-3 model after 2010 we use special decoder as engine number is reused
+                /*             
+        A = Diesel biturbo (Z19DTR) 130 HP
+        B = Diesel biturbo (Z19DTR) 160 HP
+        D = Diesel biturbo (Z19DTR) 180 HP
+        F = 1.8t (B207E)
+        N = 2.0 T/163/BP (B207H)
+        M = 2.0 T/163 (B207G)
+        S = 2.0t (B207L)
+        T = 2.0 TS/BP (B207S)
+        U = 2.0 T/BP (B207M)
+        X = 2.0 LPT/BP (B207F)
+        Y = 2.0 Turbo (B207R)
+                     */
+                if (VINNumber[7] == 'A') return VINEngineType.Z19DTR_130;
+                else if (VINNumber[7] == 'B') return VINEngineType.Z19DTR_160;
+                else if (VINNumber[7] == 'D') return VINEngineType.Z19DTR_180;
+                else if (VINNumber[7] == 'F') return VINEngineType.B207E;
+                else if (VINNumber[7] == 'N') return VINEngineType.B207H;
+                else if (VINNumber[7] == 'M') return VINEngineType.B207G;
+                else if (VINNumber[7] == 'S') return VINEngineType.B207L;
+                else if (VINNumber[7] == 'T') return VINEngineType.B207S;
+                else if (VINNumber[7] == 'U') return VINEngineType.B207M;
+                else if (VINNumber[7] == 'X') return VINEngineType.B207F;
+                else if (VINNumber[7] == 'Y') return VINEngineType.B207R;
+                else return VINEngineType.Unknown;
+            }
+            else if (makeYear == 2010)
+            {
+                if (VINNumber[7] == 'F') return VINEngineType.B207E;
+                else if (VINNumber[7] == 'S') return VINEngineType.B207L;
+                else if (VINNumber[7] == 'Y') return VINEngineType.B207R;
+                else if (VINNumber[7] == 'C') return VINEngineType.B205E;
+                else if (VINNumber[7] == 'E') return VINEngineType.B235E;
+                else if (VINNumber[7] == 'G') return VINEngineType.B235R;
+                else if (VINNumber[7] == 'Z') return VINEngineType.A20NHT_LDK;
+                else if (VINNumber[7] == 'R') return VINEngineType.B284R;
+                else if (VINNumber[7] == 'H') return VINEngineType.A28NET_LBW;
+                else if (VINNumber[7] == 'J') return VINEngineType.A28NER_LAU;
+                else if (VINNumber[7] == 'V') return VINEngineType.Z19DT;
+                else if (VINNumber[7] == 'W') return VINEngineType.Z19DTH;
+                else if (VINNumber[7] == 'P') return VINEngineType.Z19DTR;
+                else if (VINNumber[7] == 'W') return VINEngineType.A20DTH_LBS;
+                else if (VINNumber[7] == 'P') return VINEngineType.A20DTR_LBY;
+                
+            }
+            else if (makeYear == 2011)
+            {
+                if (VINNumber[7] == 'C') return VINEngineType.A20NFT;
+                else if (VINNumber[7] == 'H') return VINEngineType.A28NET_LBW;
+                else if (VINNumber[7] == 'J') return VINEngineType.A28NER_LAU;
+                else if (VINNumber[7] == 'M') return VINEngineType.B207G;
+                else if (VINNumber[7] == 'R') return VINEngineType.A20NFT;
+                else if (VINNumber[7] == 'T') return VINEngineType.B207S;
+                else if (VINNumber[7] == 'U') return VINEngineType.B207M;
+                else if (VINNumber[7] == 'X') return VINEngineType.B207F;
+            }
+            
+
+            // For all models and 9-3 before MY2011
+            if (VINNumber[7] == 'A') return VINEngineType.B235L;
             else if (VINNumber[7] == 'B') return VINEngineType.Z18XE;
             else if (VINNumber[7] == 'C') return VINEngineType.B205E;
             else if (VINNumber[7] == 'D') return VINEngineType.D223L;
@@ -305,16 +439,18 @@ namespace T7
             else if (VINNumber[7] == 'K') return VINEngineType.B205R;
             else if (VINNumber[7] == 'L') return VINEngineType.D308L;
             else if (VINNumber[7] == 'M') return VINEngineType.B284E;
+            else if (VINNumber[7] == 'N') return VINEngineType.B204L;
             else if (VINNumber[7] == 'P') return VINEngineType.Z19DTR;
             else if (VINNumber[7] == 'R') return VINEngineType.B284R;
             else if (VINNumber[7] == 'S') return VINEngineType.B207L;
+            else if (VINNumber[7] == 'T') return VINEngineType.B204E;
             else if (VINNumber[7] == 'U') return VINEngineType.B284L;
-            else if (VINNumber[7] == 'V') return VINEngineType.B207L;
-            else if (VINNumber[7] == 'W') return VINEngineType.Z19DT;
-            else if (VINNumber[7] == 'Y') return VINEngineType.Z19DTH;
+            else if (VINNumber[7] == 'V') return VINEngineType.Z19DT;
+            else if (VINNumber[7] == 'W') return VINEngineType.Z19DTH;
+            else if (VINNumber[7] == 'Y') return VINEngineType.B207R;
             else if (VINNumber[7] == 'Z') return VINEngineType.B308E;
 
-            /*
+        /*
 B = Fuel Injection, B234 (2.3 liter 16v) 
 D = Fuel Injection, B202 (16-v) 
 E = Fuel Injection, B212 (2.1 liter 16v) 
@@ -325,7 +461,7 @@ N = Turbo, B204 & intercooler (2.0 liter 16v with balance shafts)
 S = Turbo, B201 (8-valve) 
 S = Turbo, B202 Low-pressure turbo (16-valve) 
 V = 2.5 liter V-6 
-             * */
+         * */
             return VINEngineType.Unknown;
         }
 
@@ -354,6 +490,8 @@ V = 2.5 liter V-6
             else if (VINNumber[9] == 'A') return 2010;
             else if (VINNumber[9] == 'B') return 2011;
             else if (VINNumber[9] == 'C') return 2012;
+            else if (VINNumber[9] == 'D') return 2013;
+            else if (VINNumber[9] == 'E') return 2014;
             return 0;
         }
     }
@@ -429,5 +567,6 @@ V = 2.5 liter V-6
             get { return _gearboxDescription; }
             set { _gearboxDescription = value; }
         }
+
     }
 }
