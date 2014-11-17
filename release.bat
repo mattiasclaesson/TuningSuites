2cb2836500f7e4f37d7c0a20390f427008213673
mattiasclaesson <mattiaserikanna@gmail.com>
2014-11-17 21:12:48 +0100

    New install path, urls and version.

--- release.bat
+++ release.bat
@@ -4,4 +4,7 @@ mkdir C:\users\mattias\Dropbox\public\T5Suite2\%T5SuiteII.version%
 xcopy T5SuiteII\version.bat C:\users\mattias\Dropbox\public\T5Suite2\%T5SuiteII.version%\
 xcopy T5SuiteII\Release\T5SuiteII.msi C:\users\mattias\Dropbox\public\T5Suite2\%T5SuiteII.version%\
 
+echo ^<?xml version="1.0" encoding="utf-8"?^>  > C:\users\mattias\Dropbox\public\T5Suite2\version.xml
+echo ^<t7suitepro version="%T5SuiteII.version%"/^> >> C:\users\mattias\Dropbox\public\T5Suite2\version.xml
+
 git tag T5SuiteII_v%T5SuiteII.version%
