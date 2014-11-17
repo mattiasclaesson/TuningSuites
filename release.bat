932146b4209eed9a2235a45aa0ed15cde5d448ac
mattiasclaesson <mattiaserikanna@gmail.com>
2014-11-17 21:45:52 +0100

    Fixed mistake in release,

--- release.bat
+++ release.bat
@@ -5,6 +5,5 @@ xcopy T5SuiteII\version.bat C:\users\mattias\Dropbox\public\T5Suite2\%T5SuiteII.
 xcopy T5SuiteII\Release\T5SuiteII.msi C:\users\mattias\Dropbox\public\T5Suite2\%T5SuiteII.version%\
 
 echo ^<?xml version="1.0" encoding="utf-8"?^>  > C:\users\mattias\Dropbox\public\T5Suite2\version.xml
-echo ^<t7suitepro version="%T5SuiteII.version%"/^> >> C:\users\mattias\Dropbox\public\T5Suite2\version.xml
-
+echo ^<t5suite2 version="%T5SuiteII.version%"/^> >> C:\users\mattias\Dropbox\public\T5Suite2\version.xml
 git tag T5SuiteII_v%T5SuiteII.version%
