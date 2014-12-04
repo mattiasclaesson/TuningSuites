93be4d2c3081c386fe994ccfa569c7c7108abe26
mattiasclaesson <mattiaserikanna@gmail.com>
2014-12-04 22:54:27 +0100

    New versions.

--- T8/Properties/AssemblyInfo.cs
+++ T8/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.16.0")]
-[assembly: AssemblyFileVersion("0.0.16.0")]
+[assembly: AssemblyVersion("0.0.17.0")]
+[assembly: AssemblyFileVersion("0.0.17.0")]
