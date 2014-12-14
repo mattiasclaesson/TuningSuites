f15f6f94174108393369e5c45109183ed199dba6
mattiasclaesson <mattiaserikanna@gmail.com>
2014-12-14 23:42:14 +0100

    New versions. New TrionicCANlib

--- SetupT8SuitePro/version.bat
+++ SetupT8SuitePro/version.bat
@@ -1,7 +1,7 @@
 @echo off
-set T8.version=0.0.18.0
-set SetupT8Suite.version=0.0.18
-set TrionicCANLib.version=0.0.0.16
+set T8.version=0.0.19.0
+set SetupT8Suite.version=0.0.19
+set TrionicCANLib.version=0.0.20.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
 set LBIndustrialCtrls.version=1.5.7.0
--- T8/Properties/AssemblyInfo.cs
+++ T8/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.18.0")]
-[assembly: AssemblyFileVersion("0.0.18.0")]
+[assembly: AssemblyVersion("0.0.19.0")]
+[assembly: AssemblyFileVersion("0.0.19.0")]
