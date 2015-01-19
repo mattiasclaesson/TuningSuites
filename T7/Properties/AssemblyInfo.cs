842f6bd8a1fb4e70da95107794a7e4778f789186
mattiasclaesson <mattiaserikanna@gmail.com>
2015-01-19 19:30:41 +0100

    New version fixed issue #6, freeze during realtime and afr

--- T7/Properties/AssemblyInfo.cs
+++ T7/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MattiasC")]
 [assembly: AssemblyProduct("T7")]
-[assembly: AssemblyCopyright("Copyright © 2014 Dilemma/Mattias Claesson")]
+[assembly: AssemblyCopyright("Copyright © 2015 Dilemma/Mattias Claesson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.21.0")]
-[assembly: AssemblyFileVersion("0.0.21.0")]
+[assembly: AssemblyVersion("0.0.22.0")]
+[assembly: AssemblyFileVersion("0.0.22.0")]
