7bac406a8d20b2c6b068fea5cfb65d548b054f46
mattiasclaesson <mattiaserikanna@gmail.com>
2015-05-07 22:23:49 +0200

    Added t8x packs to the installer. New versions.

--- SetupT8SuitePro/version.bat
+++ SetupT8SuitePro/version.bat
@@ -1,6 +1,6 @@
 @echo off
-set T8.version=0.0.23.0
-set SetupT8Suite.version=0.0.23
+set T8.version=0.0.24.0
+set SetupT8Suite.version=0.0.24
 set TrionicCANLib.version=0.0.20.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
