bb84f07da6c23418d95e883ed33e81a9c068092d
mattiasclaesson <mattiaserikanna@gmail.com>
2015-06-11 20:10:00 +0200

    New version

--- T7/Properties/AssemblyInfo.cs
+++ T7/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.23.0")]
-[assembly: AssemblyFileVersion("0.0.23.0")]
+[assembly: AssemblyVersion("0.0.24.0")]
+[assembly: AssemblyFileVersion("0.0.24.0")]
