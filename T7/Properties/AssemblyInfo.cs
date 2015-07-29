d6cd405df510c6659c23bc7720ff4eb95a6e8444
mattiasclaesson <mattiaserikanna@gmail.com>
2015-07-29 17:53:04 +0200

    New versions. Added support URLs, set removepreviousversions=true in installer.

--- T7/Properties/AssemblyInfo.cs
+++ T7/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.30.0")]
-[assembly: AssemblyFileVersion("0.0.30.0")]
+[assembly: AssemblyVersion("0.0.31.0")]
+[assembly: AssemblyFileVersion("0.0.31.0")]
