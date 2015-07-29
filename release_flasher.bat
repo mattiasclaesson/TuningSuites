5432b5f430b8acad88e06379b5ae5f0c9caf86da
mattiasclaesson <mattiaserikanna@gmail.com>
2015-07-29 16:03:58 +0200

    Fixed release script

--- release_flasher.bat
+++ release_flasher.bat
@@ -1,17 +1,18 @@
 call T5CanFlash\version.bat
-devenv T5CanFlasher.sln /Rebuild Release /project T5CanFlasher
+devenv T5CanFlasher.sln /Rebuild Release /project T5CanFlash
 
 pushd T5CanFlash\Release\
-"C:\Program Files (x86)\hashutils-1.3.0-redist\bin.x86-32\md5sum.exe" T5CanFlasher.msi >> T5CanFlasher.md5
+"C:\Program Files (x86)\hashutils-1.3.0-redist\bin.x86-32\md5sum.exe" T5CanFlash.msi >> T5CanFlash.md5
 popd
 
-mkdir C:\users\mattias\Dropbox\public\T5CanFlash\%T5CanFlasher.version%
-xcopy T5CanFlash\version.bat C:\users\mattias\Dropbox\public\T5CanFlasher\%T5CanFlasher.version%\
-xcopy T5CanFlash\Release\T5CanFlasher.msi C:\users\mattias\Dropbox\public\T5CanFlasher\%T5CanFlasher.version%\
-xcopy T5CanFlash\Release\T5CanFlasher.md5 C:\users\mattias\Dropbox\public\T5CanFlasher\%T5CanFlasher.version%\
+mkdir C:\users\mattias\Dropbox\public\T5CanFlasher\%T5CanFlash.version%
+xcopy T5CanFlash\version.bat C:\users\mattias\Dropbox\public\T5CanFlasher\%T5CanFlash.version%\
+xcopy T5CanFlash\Release\T5CanFlash.msi C:\users\mattias\Dropbox\public\T5CanFlasher\%T5CanFlash.version%\
+xcopy T5CanFlash\Release\T5CanFlash.md5 C:\users\mattias\Dropbox\public\T5CanFlasher\%T5CanFlash.version%\
 
 echo ^<?xml version="1.0" encoding="utf-8"?^>  > C:\users\mattias\Dropbox\public\T5CanFlasher\version.xml
-echo ^<t5canflasher version="%T5CanFlasher.version%"/^> >> C:\users\mattias\Dropbox\public\T5CanFlasher\version.xml
+echo ^<t5canflasher version="%T5CanFlash.version%"/^> >> C:\users\mattias\Dropbox\public\T5CanFlasher\version.xml
+
 git tag T5CanFlasher_v%T5CanFlasher.version%
 git tag T5CanLib_v%T5CanLib.version%
 git tag SetupT5CanFlash_v%T5CanFlash.version%
