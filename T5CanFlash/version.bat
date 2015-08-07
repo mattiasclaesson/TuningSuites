bda07511fb95fde135975c70fb788d7330421c4c
mattiasclaesson <mattiaserikanna@gmail.com>
2015-08-08 00:09:50 +0200

    New flasher version

--- T5CanFlash/version.bat
+++ T5CanFlash/version.bat
@@ -1,4 +1,4 @@
 @echo off
-set T5CanFlasher.version=2.0.3.0
-set T5CanLib.version=2.0.1.0
-set T5CanFlash.version=2.0.3
+set T5CanFlash.version=2.0.4
+set T5CanFlasher.version=2.0.4.0
+set T5CanLib.version=2.0.2.0
