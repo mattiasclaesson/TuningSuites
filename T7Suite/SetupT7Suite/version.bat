227671d2bcbe9ef716715bcbe737021c85e32f0f
mattiasclaesson <mattiaserikanna@gmail.com>
2015-10-09 09:49:45 +0200

    New version

--- T7Suite/SetupT7Suite/version.bat
+++ T7Suite/SetupT7Suite/version.bat
@@ -1,6 +1,6 @@
 @echo off
-set T7.version=0.1.4.0
-set SetupT7Suite.version=0.1.4
+set T7.version=0.1.5.0
+set SetupT7Suite.version=0.1.5
 set TrionicCANLib.version=0.1.15.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
