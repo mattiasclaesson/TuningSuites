71335b5ba28ad055766c8de8602cbecc5a5244ae
mattiasclaesson <mattiaserikanna@gmail.com>
2016-04-17 00:34:06 +0200

    New t8suite version

--- T8Suite/Properties/AssemblyInfo.cs
+++ T8Suite/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.14.0")]
-[assembly: AssemblyFileVersion("0.1.14.0")]
+[assembly: AssemblyVersion("0.1.15.0")]
+[assembly: AssemblyFileVersion("0.1.15.0")]
