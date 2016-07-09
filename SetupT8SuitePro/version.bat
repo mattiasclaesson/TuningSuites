9049841401107dc34a450a63c559faa34d6e261e
mattiasclaesson <mattiaserikanna@gmail.com>
2016-07-09 10:45:43 +0200

    New trioniccanlib with kvaser fixes

--- SetupT8SuitePro/version.bat
+++ SetupT8SuitePro/version.bat
@@ -1,7 +1,7 @@
 @echo off
 set T8.version=0.1.26.0
 set SetupT8Suite.version=0.1.26
-set TrionicCANLib.version=0.1.27.0
+set TrionicCANLib.version=0.1.30.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
 set LBIndustrialCtrls.version=1.5.7.0
