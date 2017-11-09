77f96daa4567ac2642ec92cb0cceb143c375159c
Mattias Claesson <mattiaserikanna@gmail.com>
2017-11-09 19:36:38 +0100

    New T7suite version

--- SetupT7Suite/version.bat
+++ SetupT7Suite/version.bat
@@ -1,6 +1,6 @@
 @echo off
-set T7.version=0.1.39.0
-set SetupT7Suite.version=0.1.39
+set T7.version=0.1.40.0
+set SetupT7Suite.version=0.1.40
 set TrionicCANLib.version=0.1.41.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
@@ -9,8 +9,6 @@ set Owf.Controls.DigitalDisplayControl=1.0.1.0
 set ProCharts.version=1.0.0.0
 set ProGauges.version=1.5.7.0
 set RealtimeGraph.version=1.8.7.0
-set T7CANFlasher.version=2.5.6.0
-set WakeupCANbus.version=2.5.6.0
 set SuiteLauncher.version=3.0.1.0
 set SaabOpenTech.version=0.40
 set Be.Windows.Forms.HexBox.version=1.2.0.14173
@@ -20,4 +18,4 @@ set FTD2XX_NET.version=1.0.10.0
 set LibUsbDotNet.version=2.2.8.104
 set PSTaskDialog.version=1.0.0.0
 set Wideband.version=1.0.2.0
-set CommonSuite.version=1.0.6.0
+set CommonSuite.version=1.0.7.0
