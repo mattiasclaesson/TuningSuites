03f3356cbb98741053e36b7e550f20fc49db9e36
Mattias Claesson <mattiaserikanna@gmail.com>
2017-11-13 21:26:51 +0100

    New t8suite version

--- SetupT8SuitePro/version.bat
+++ SetupT8SuitePro/version.bat
@@ -1,6 +1,6 @@
 @echo off
-set T8.version=0.1.37.0
-set SetupT8Suite.version=0.1.37
+set T8.version=0.1.38.0
+set SetupT8Suite.version=0.1.38
 set TrionicCANLib.version=0.1.41.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
@@ -8,7 +8,7 @@ set LBIndustrialCtrls.version=1.5.7.0
 set Owf.Controls.DigitalDisplayControl=2.0.1.0
 set ProCharts.version=1.0.0.0
 set ProGauges.version=1.5.7.0
-set RealtimeGraph.version=1.8.7.0
+set RealtimeGraph.version=3.0.0.0
 set SuiteLauncher.version=3.0.1.0
 set Be.Windows.Forms.HexBox.version=1.2.0.14173
 set canusbdrv_net.version=2.0.0.1
@@ -18,4 +18,4 @@ set LibUsbDotNet.version=2.2.8.104
 set PSTaskDialog.version=1.0.0.0
 set ICSharpCode.SharpZipLib=0.86.0.518
 set Wideband.version=1.0.2.0
-set CommonSuite.version=1.0.7.0
+set CommonSuite.version=1.0.8.0
