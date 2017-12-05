09cb78c428fbe61fdb9b88b5d365e32e3833fae5
mattiasclaesson <mattiaserikanna@gmail.com>
2017-12-06 00:13:08 +0100

    Once again fix the T5Suite release script

--- releaseT5Suite.bat
+++ releaseT5Suite.bat
@@ -1,14 +1,14 @@
-call T5SuiteII\version.bat
+call SetupT5SuiteII\version.bat
 devenv T5SuiteII.sln /Rebuild Release /project SetupT5SuiteII
 
-pushd T5SuiteII\Release\
+pushd SetupT5SuiteII\Release\
 "C:\md5sum.exe" T5SuiteII.msi >> T5SuiteII.md5
 popd
 
 mkdir z:\T5Suite2\%T5SuiteII.version%
-xcopy T5SuiteII\version.bat z:\T5Suite2\%T5SuiteII.version%\
-xcopy T5SuiteII\Release\T5SuiteII.msi z:\T5Suite2\%T5SuiteII.version%\
-xcopy T5SuiteII\Release\T5SuiteII.md5 z:\T5Suite2\%T5SuiteII.version%\
+xcopy SetupT5SuiteII\version.bat z:\T5Suite2\%T5SuiteII.version%\
+xcopy SetupT5SuiteII\Release\T5SuiteII.msi z:\T5Suite2\%T5SuiteII.version%\
+xcopy SetupT5SuiteII\Release\T5SuiteII.md5 z:\T5Suite2\%T5SuiteII.version%\
 
 echo ^<?xml version="1.0" encoding="utf-8"?^>  > z:\T5Suite2\version.xml
 echo ^<t5suite2 version="%T5SuiteII.version%"/^> >> z:\T5Suite2\version.xml
