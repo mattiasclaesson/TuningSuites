5eda6f04f8252ea29d0540d374fb326924ac0be3
mattiasclaesson <mattiaserikanna@gmail.com>
2018-09-23 15:38:37 +0200

    T8suite new version

--- SetupT8SuitePro/version.bat
+++ SetupT8SuitePro/version.bat
@@ -1,6 +1,6 @@
 @echo off
-set T8.version=0.1.42.0
-set SetupT8Suite.version=0.1.42
+set T8.version=0.1.43.0
+set SetupT8Suite.version=0.1.43
 set TrionicCANLib.version=0.1.50.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
