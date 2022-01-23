b27d68942c129b763ecf49b4260aba2f37288626
Mattias Claesson <mattiaserikanna@gmail.com>
2022-01-23 20:43:46 +0100

    New t8suite version

--- SetupT8SuitePro/version.bat
+++ SetupT8SuitePro/version.bat
@@ -1,7 +1,7 @@
 @echo off
-set T8.version=0.1.53.0
-set SetupT8Suite.version=0.1.53
-set TrionicCANLib.version=0.1.56.0
+set T8.version=0.1.54.0
+set SetupT8Suite.version=0.1.54
+set TrionicCANLib.version=0.1.58.0
 set AquaGauge.version=1.5.7.0
 set ICSharpCode.TextEditor.version=3.0.0.3437
 set LBIndustrialCtrls.version=1.5.7.0
