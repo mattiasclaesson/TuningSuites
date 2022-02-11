ecb20534fb7351cd0208562a860574416f0d7a82
Mattias Claesson <mattiaserikanna@gmail.com>
2022-02-11 23:35:29 +0100

    New t7suite version

--- SetupT8SuitePro/version.bat
+++ SetupT8SuitePro/version.bat
@@ -16,5 +16,5 @@ set FTD2XX_NET.version=1.0.10.0
 set LibUsbDotNet.version=2.2.8.104
 set PSTaskDialog.version=1.0.0.0
 set ICSharpCode.SharpZipLib=0.86.0.518
-set Wideband.version=1.0.3.0
+set Wideband.version=1.0.4.0
 set CommonSuite.version=1.0.15.0
